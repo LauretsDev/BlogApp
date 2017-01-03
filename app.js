0b0939fc6f281b816bf6d6eb5cd11fc44dff7e0d
Diego Laurentino <diego.r.laurentino@gmail.com>
2017-01-03 00:56:15 -0200

    Added create route and changed some views.

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.get('/', function(req, res) {
     res.redirect('/blogs');
 });
 
+// INDEX ROUTE
 app.get('/blogs', function(req, res) {
 	Blog.find({}, function(err, blogs) {
 		if(err) {
@@ -32,6 +33,40 @@ app.get('/blogs', function(req, res) {
 			res.render('index', {blogs: blogs});
 		}
 	})
+});
+
+// NEW ROUTE
+app.get('/blogs/new',function(req,res) {
+    res.render('new');
+});
+
+// CREATE ROUTE
+app.post('/blogs',function(req,res) {
+    Blog.create(req.body.blog, function(err, newBlog) {
+    	if(err) {
+    		res.render('new');
+    	}
+    	else {
+    		res.redirect('/blogs');
+    	}
+    });
+});
+
+// SHOW ROUTE
+app.get('/blogs/:id',function(req,res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+    	if(err) {
+    		res.redirect("/blogs");
+    	}
+    	else {
+    		res.render('show', {blog: foundBlog});
+    	}
+    });
+});
+
+// EDIT ROUTE
+app.get('/blogs/:id/edit',function(req,res) {
+    res.render('edit');
 });
 
 app.listen(3000, 'localhost', function() {
